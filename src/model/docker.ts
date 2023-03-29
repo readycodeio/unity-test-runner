cf2af9a42fc2f312f82ce320ea4bcb3eb6c80fc7
Julek Kopczewski <julek.kopczewski@readycode.io>
2023-03-29 02:16:08 +0200

    Adding privilege

--- src/model/docker.ts
+++ src/model/docker.ts
@@ -113,6 +113,7 @@ const Docker = {
                 --env GIT_PRIVATE_TOKEN="${gitPrivateToken}" \
                 --env CHOWN_FILES_TO="${chownFilesTo}" \
                 --shm-size=128m \
+                --privileged \
                 ${sshAgent ? '--env SSH_AUTH_SOCK=/ssh-agent' : ''} \
                 --volume "${githubHome}:/root:z" \
                 --volume "${githubWorkflow}:/github/workflow:z" \
